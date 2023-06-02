3192e8ba995d49d978d8ea845788af5ac4547865
Bismark -Le Analyst <49318607+bismarkquaye@users.noreply.github.com>
2023-06-02 01:39:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navi-top');
+    
+    if (window.pageYOffset > 0) {
+      navbar.classList.add('fixed');
+    } else {
+      navbar.classList.remove('fixed');
+    }
+  });
+  
+  $('.box').click(function() {
+    // Action to perform when the div is clicked
+    $(this).addClass('clicked');
+    setTimeout(function() {
+        $(this).removeClass('clicked');
+      }, 500);
+    window.open('http://www.google.com', '_blank');
+})
+
+const texts = ["Hello", "How are you", "I'm doing well"];
+const typingText = document.getElementById("typing-text");
+let index = 0;
+
+function typeText() {
+  const currentText = texts[index];
+  typingText.textContent = ""; // Clear previous text
+  animateText(currentText, 0);
+  index = (index + 1) % texts.length; // Move to the next text
+}
+
+function animateText(text, charIndex) {
+  if (charIndex < text.length) {
+    typingText.textContent += text.charAt(charIndex);
+    charIndex++;
+    setTimeout(() => animateText(text, charIndex), 100);
+  } else {
+    // Pause for a duration before moving to the next text
+    setTimeout(typeText, 2000);
+  }
+}
+
+typeText();
